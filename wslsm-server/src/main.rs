2c4f836af950f7c470ac0e35695b81a9272ab12e
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-04-29 10:40:59 -0700

    feat(server): run any executables using message

--- wslsm-server/src/main.rs
+++ wslsm-server/src/main.rs
@@ -3,7 +3,6 @@ use std::process::Command;
 use std::thread::spawn;
 use tungstenite::{accept, Message};
 
-/// A WebSocket echo server
 fn main() {
     let server = TcpListener::bind("127.0.0.1:9001").unwrap();
     for stream in server.incoming() {
@@ -11,12 +10,13 @@ fn main() {
             let mut websocket = accept(stream.unwrap()).unwrap();
             loop {
                 let msg = websocket.read_message().unwrap();
-                if msg.is_binary() || msg.is_text() {
-                    let output = Command::new("./bin/cpu_usage").output().unwrap();
-                    let message =
-                        Message::Text(String::from_utf8_lossy(&output.stdout).to_string());
-                    websocket.write_message(message).unwrap();
-                }
+                let msg_str = match msg {
+                    Message::Text(text) => text,
+                    _ => String::new(),
+                };
+                let output = Command::new(format!("./bin/{}", msg_str)).output().unwrap();
+                let message = Message::Text(String::from_utf8_lossy(&output.stdout).to_string());
+                websocket.write_message(message).unwrap();
             }
         });
     }
