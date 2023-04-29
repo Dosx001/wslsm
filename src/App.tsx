6d5bfd2c28ea1ff8d64981fac4829ef52f83d36b
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-04-29 11:37:46 -0700

    feat(solid): add used_mem

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Component, createSignal, onCleanup } from "solid-js";
 const App: Component = () => {
   const [cpufequ, setCpufequ] = createSignal("");
   const [totalMem, setTotalMem] = createSignal("");
+  const [usedMem, setUsedMem] = createSignal("");
   const socket = new WebSocket("ws://127.0.0.1:9001");
   socket.addEventListener("open", () => {
     socket.send("cpu_usage");
     socket.send("total_mem");
+    socket.send("used_mem");
   });
   socket.addEventListener("message", (ev) => {
     const resp = JSON.parse(ev.data);
@@ -17,17 +19,24 @@ const App: Component = () => {
       case "total_mem":
         setTotalMem(resp.data);
         break;
+      case "used_mem":
+        setUsedMem(resp.data);
+        break;
     }
   });
   socket.addEventListener("close", () => {
     console.debug("WebSocket connection closed.");
   });
-  const id = setInterval(() => socket.send("cpu_usage"), 250);
+  const id = setInterval(() => {
+    socket.send("cpu_usage");
+    socket.send("used_mem");
+  }, 250);
   onCleanup(() => clearInterval(id));
   return (
     <div>
       <p>Avg CPU usage: {cpufequ()}%</p>
       <p>Total memory: {totalMem()}GB</p>
+      <p>Used memory: {usedMem()}GB</p>
     </div>
   );
 };
