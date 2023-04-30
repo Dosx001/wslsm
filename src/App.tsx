ae92f7bf28964b65ffdcb30051a10c9c66dba3f2
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-04-30 09:36:45 -0700

    feat(solid): make plot of avg cpu usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Component, createSignal, onCleanup } from "solid-js";
 import CPUinfo from "./components/CPUinfo";
-import Graph from "./Graph";
 
 const App: Component = () => {
   const [totalMem, setTotalMem] = createSignal("");
   const [usedMem, setUsedMem] = createSignal("");
   const socket = new WebSocket("ws://127.0.0.1:9001");
   socket.addEventListener("open", () => {
-    socket.send("cpu_usage");
     socket.send("total_mem");
     socket.send("used_mem");
   });
@@ -26,7 +24,6 @@ const App: Component = () => {
     console.debug("WebSocket connection closed.");
   });
   const id = setInterval(() => {
-    socket.send("cpu_usage");
     socket.send("used_mem");
   }, 250);
   onCleanup(() => clearInterval(id));
@@ -34,7 +31,6 @@ const App: Component = () => {
     <div>
       <p>Total memory: {totalMem()}GB</p>
       <p>Used memory: {usedMem()}GB</p>
-      <Graph />
       <CPUinfo />
     </div>
   );
--- src/components/CPUinfo.tsx
+++ src/components/CPUinfo.tsx
@@ -1,27 +1,62 @@
-import { createSignal, onCleanup } from "solid-js";
+import * as d3 from "d3";
+import { createEffect, createSignal } from "solid-js";
 
 const CPUinfo = () => {
   const [cpufequ, setCpufequ] = createSignal("");
-  const socket = new WebSocket("ws://127.0.0.1:9001");
-  socket.addEventListener("open", () => {
-    socket.send("cpu_usage");
+  let div!: HTMLDivElement;
+  const data: number[] = [];
+  createEffect(() => {
+    const width = 500;
+    const height = 300;
+    const svg = d3
+      .select(div)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, width]);
+    const yScale = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+    const line = d3
+      .line()
+      .x((_, i) => xScale(i))
+      .y((d) => yScale(d));
+    svg
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+    const addDataPoint = () => {
+      xScale.domain([0, data.length - 1]);
+      yScale.domain([0, 100]);
+      svg.select("path").datum(data).attr("d", line);
+    };
+    const socket = new WebSocket("ws://127.0.0.1:9001");
+    socket.addEventListener("open", () => {
+      socket.send("cpu_usage");
+    });
+    socket.addEventListener("message", (ev) => {
+      const resp = JSON.parse(ev.data);
+      switch (resp.type) {
+        case "cpu_usage":
+          setCpufequ(resp.data);
+          if (data.length === 100) data.shift();
+          data.push(resp.data);
+          addDataPoint();
+          break;
+      }
+    });
+    setInterval(() => {
+      socket.send("cpu_usage");
+    }, 250);
   });
-  socket.addEventListener("message", (ev) => {
-    const resp = JSON.parse(ev.data);
-    switch (resp.type) {
-      case "cpu_usage":
-        setCpufequ(resp.data);
-        break;
-    }
-  });
-  const id = setInterval(() => {
-    socket.send("cpu_usage");
-  }, 250);
-  onCleanup(() => clearInterval(id));
   return (
-    <>
+    <div ref={div}>
       <p>Avg CPU usage: {cpufequ()}%</p>
-    </>
+    </div>
   );
 };
 
